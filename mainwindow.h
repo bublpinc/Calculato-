795c271f67fd80320605183094ec9255c2ae1755
AriMalc <arisha.malceva1243@gmail.com>
2018-06-20 17:28:43 +0700

    All files was added

---
+++ mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void digits_numbers();
+    void on_pushButton_dot_clicked();
+    void operations();
+    void on_pushButton_clicked();
+    void on_pushButton_equally_clicked();
+    void math_operation();
+};
+
+#endif // MAINWINDOW_H
